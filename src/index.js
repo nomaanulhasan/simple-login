1b6697467878ce974fb311094e24d780bc67324a
Umar <github@nomaanulhasan.com>
2020-09-08 14:50:03 +0530

    added simple login functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { LandingPage } from './landing.page';
+import { AppLayout } from './app.layout';
+import { ProtectedRoute } from './protected.route';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
+import './styles.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={LandingPage} />
+				<ProtectedRoute
+					exact
+					path="/app"
+					component={AppLayout}
+				/>
+				<Route
+					path=""
+					component={() => {
+						return <h1>404 NOT FOUND</h1>;
+					}}
+				/>
+			</Switch>
+		</div>
+	);
+}
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
